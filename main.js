37b8aa7b93bb10b631f86cdc666b878f02f7f445
siverain02 <ekdldkaa@naver.com>
2023-02-15 21:51:53 +0900

    uri setting

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ var path = require('path');
 var sanitizeHtml = require('sanitize-html');
 var mysql = require('mysql');
 
+//프론트 페이지 위치
+var template = require('./lib/template.js');
+
 //connect DB
 /*
 var db = mysql.createConnection({
@@ -18,14 +21,54 @@ var db = mysql.createConnection({
 db.connect()
 */
 
-//application sample
+//application
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var pathname = url.parse(_url, true).pathname;
     if(pathname === '/'){
+
+        //main페이지
         response.writeHead(200);
-        response.end('Hello World');
+        response.end('This is main');
+        
+    }else if(pathname === '/navigation'){
+
+        //navigation페이지
+        response.writeHead(200);
+        response.end('This is navigation');
+
+    }else if(pathname === '/auth/join'){
+
+        //회원가입페이지
+        response.writeHead(200);
+        response.end('This is join');
+
+    }else if(pathname === '/auth/login'){
+
+        //로그인페이지
+        response.writeHead(200);
+        response.end('This is login');
+        
+    }else if(pathname === '/profile'){
+
+        //회원가입페이지
+        response.writeHead(200);
+        response.end('This is profile');
+        
+    }else if(pathname === '/exercise'){
+
+        //운동기록
+        response.writeHead(200);
+        response.end('This is exercise');
+        
+    }else if(pathname === '/diet'){
+
+        //식단기록
+        response.writeHead(200);
+        response.end('This is diet');
+        
     }
+
 });
 
 app.listen(3000);
