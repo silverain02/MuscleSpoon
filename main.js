cdf1a6cd22e0e8a31ac41e753eff29035937c3b2
yuchael <97613473+yuchael@users.noreply.github.com>
2023-02-26 18:14:26 +0900

    일일 페이지 기능

--- main.js
+++ main.js
@@ -20,6 +20,8 @@ db.connect();
 var template_greeter = require('./lib/greeter.js');
 var template_navigation = require('./lib/navigation.js');
 var template_profile = require('./lib/profile.js');
+var template_exercise = require('./lib/exercise.js');
+var template_diet = require('./lib/diet.js');
 // var template_login = require('./lib/login.js');
 
 //application
@@ -240,18 +242,154 @@ var app = http.createServer(function(request,response){
         })
 
 
-    }else if(pathname === '/exercise'){
+    }else if(pathname === '/exercise' && method === 'GET'){
+        //운동기록 페이지
+        db.query(`SELECT * FROM exercise WHERE userId=? AND date=?`, [queryData.userId, new Date().toISOString().slice(0, 10)], function(error,exerData){
+            if(error){
+                throw error;
+            }
+            var list = template_exercise.list(exerData);
+            var html = template_exercise.HTML(list);
+            
+            response.writeHead(200);
+            response.end(html);
+        });
+    }else if(pathname === '/exercise' && method === 'POST'){
+       //운동기록 정보 db에 넣기
+       const body = [];
+            
+       request.on('data', (data) => {
+        body.push(data);
+       })
 
-        //운동기록
-        response.writeHead(200);
-        response.end('This is exercise');
-        
-    }else if(pathname === '/diet'){
+       request.on('end', () => {
+        const parsedBody = Buffer.concat(body).toString();
 
-        //식단기록
-        response.writeHead(200);
-        response.end('This is diet');
+        let part = parsedBody.split("\"")[3];
+        let exerName = parsedBody.split("\"")[7];
+        let setNum = parsedBody.split("\"")[11];
+        let date = parsedBody.split("\"")[15]; 
         
+        //같은 날짜에 동일한 운동정보가 있는지 확인
+        sql = "SELECT id FROM exercise WHERE part=? AND exerName=? AND date=? AND userID=?";
+        db.query(sql, [part, exerName, date, queryData.userId], function (err, result) {
+            if (err) throw err;
+           
+            //동일한 운동정보가 없으면
+            if (result[0] === undefined) {
+                sql = "INSERT INTO exercise (id, part, exerName, setNum, date, userID, checkYn) VALUES ?";
+                var values = [null, part, exerName, setNum, date, queryData.userId, null];
+                db.query(sql, [values], function (err, result) {
+                    if (err) throw err;
+                    response.writeHead(200);
+                    return response.end('2');
+                });
+            //동일한 운동정보가 이미 등록되어 있으면
+            }else {
+                response.writeHead(200);
+                response.end('1');
+            }
+        });
+       })
+      
+    }else if(pathname === '/exercise/delete'){
+        //운동기록 삭제
+        const body = [];
+            
+        request.on('data', (data) => {
+         body.push(data);
+        })
+
+        request.on('end', () => {
+         const parsedBody = Buffer.concat(body).toString();
+
+         let part = parsedBody.split("\"")[3];
+         let exerName = parsedBody.split("\"")[7];
+         let setNum = parsedBody.split("\"")[11]; 
+         let date = parsedBody.split("\"")[15];
+
+         sql = "DELETE FROM exercise WHERE part = ? AND exerName = ? AND setNum = ? AND date = ? AND userID=?";
+         db.query(sql, [part, exerName, setNum, date, queryData.userId], function (err, result) {
+            if (err) throw err; 
+
+            response.writeHead(200);
+            return response.end('1');
+           });
+        })
+    }else if(pathname === '/diet' && method === 'GET'){
+        //식단기록 페이지
+        db.query(`SELECT * FROM diet WHERE userId=? AND date=?`, [queryData.userId, new Date().toISOString().slice(0, 10)], function(error,dietData){
+            if(error){
+                throw error;
+            }
+            var breakfastList = template_diet.breakfastList(dietData);
+            var lunchList = template_diet.lunchList(dietData);
+            var dinnerList = template_diet.dinnerList(dietData);
+            var html = template_diet.HTML(breakfastList, lunchList, dinnerList);
+            
+            response.writeHead(200);
+            response.end(html);
+        });        
+    }else if(pathname === '/diet' && method === 'POST'){
+        // 식단 기록 정보 db에 넣기
+       const body = [];
+            
+       request.on('data', (data) => {
+        body.push(data);
+       })
+
+       request.on('end', () => {
+        const parsedBody = Buffer.concat(body).toString();
+
+        let time = parsedBody.split("\"")[3];
+        let dietName = parsedBody.split("\"")[7];
+        let gram = parsedBody.split("\"")[11]; 
+        let date = parsedBody.split("\"")[15];
+
+        //같은 날짜에 동일한 식단정보가 있는지 확인
+        sql = "SELECT id FROM diet WHERE time=? AND dietName=? AND date=? AND userID=?";
+        db.query(sql, [time, dietName, date, queryData.userId], function (err, result) {
+            if (err) throw err;
+          
+            //동일한 식단정보가 없으면
+            if (result[0] === undefined) {
+                sql = "INSERT INTO diet (id, time, dietName, gram, date, userID) VALUES ?";
+                var values = [null, time, dietName, gram, date, queryData.userId];
+                db.query(sql, values, function (err, result) {
+                    if (err) throw err;
+                    response.writeHead(200);
+                    return response.end('2');
+                });
+            //동일한 운동정보가 이미 등록되어 있으면
+            }else {
+                response.writeHead(200);
+                response.end('1');
+            }
+        });
+       })
+    }else if(pathname === '/diet/delete'){
+        //식단기록 삭제
+        const body = [];
+            
+        request.on('data', (data) => {
+         body.push(data);
+        })
+
+        request.on('end', () => {
+         const parsedBody = Buffer.concat(body).toString();
+
+         let time = parsedBody.split("\"")[3];
+         let dietName = parsedBody.split("\"")[7];
+         let date = parsedBody.split("\"")[11];
+
+         sql = "DELETE FROM diet WHERE time = ? AND dietName = ? AND date = ? AND userID = ?";
+         db.query(sql, [time, dietName, date, queryData.userId], function (err, result) {
+            if (err) throw err;  
+
+            response.writeHead(200);
+            return response.end('1');
+           });
+        })
     }else {
         response.writeHead(404);
         response.end('Not found');
